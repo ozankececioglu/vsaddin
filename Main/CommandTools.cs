79e38a06cdd12977ab8f64782941abee0c0916d4
unknown <ozan.kececioglu@PC-12.corp.simbt.com>
2014-09-05 11:39:53 +0300

    initial

---
+++ Main/CommandTools.cs
@@ -0,0 +1,209 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+#if DEBUG
+namespace CommonAddInDebug
+#else
+namespace CommonAddIn
+#endif
+{
+	static class CommonTools
+	{
+		[Command("Text Editor::Alt+Right Arrow")]
+		static void MoveRightByCamelCase(Connect connect)
+		{
+			MoveRightByCamelCaseBase(connect, false);
+		}
+
+		[Command("Text Editor::Alt+Shift+Right Arrow")]
+		static void MoveRightByCamelCaseExtend(Connect connect)
+		{
+			MoveRightByCamelCaseBase(connect, true);
+		}
+
+		[Command("Text Editor::Alt+Left Arrow")]
+		static void MoveLeftByCamelCase(Connect connect)
+		{
+			MoveLeftByCamelCaseBase(connect, false);
+		}
+
+		[Command("Text Editor::Alt+Shift+Left Arrow")]
+		static void MoveLeftByCamelCaseExtend(Connect connect)
+		{
+			MoveLeftByCamelCaseBase(connect, true);
+		}
+
+		[Command("Text Editor::Alt+BkSpce")]
+		static void DeleteLeftByCamelCase(Connect connect)
+		{
+			MoveLeftByCamelCaseBase(connect, true);
+			var selection = connect.Application.ActiveDocument.Selection as TextSelection;
+			selection.Delete();
+		}
+
+		[Command("Text Editor::Alt+Del")]
+		static void DeleteRightByCamelCase(Connect connect)
+		{
+			MoveRightByCamelCaseBase(connect, true);
+			var selection = connect.Application.ActiveDocument.Selection as TextSelection;
+			selection.Delete();
+		}
+
+		static void MoveRightByCamelCaseBase(Connect connect, bool extend)
+		{
+			var selection = connect.Application.ActiveDocument.Selection as TextSelection;
+			var point = selection.ActivePoint.CreateEditPoint();
+			point.CharRight(1);
+
+			char left, right;
+			while (true) {
+				left = GetChar(point);
+				point.CharRight(1);
+				right = GetChar(point);
+
+				if (IsCamelCase(left, right) || point.AtEndOfDocument) {
+					break;
+				}
+			}
+
+			selection.MoveToPoint(point, extend);
+		}
+
+		static void MoveLeftByCamelCaseBase(Connect connect, bool extend)
+		{
+			var selection = connect.Application.ActiveDocument.Selection as TextSelection;
+			var point = selection.ActivePoint.CreateEditPoint();
+			point.CharLeft(1);
+
+			char left, right;
+			while (true) {
+				right = GetChar(point);
+				point.CharLeft(1);
+				left = GetChar(point);
+
+				if (IsCamelCase(left, right)) {
+					point.CharRight(1);
+					break;
+				} else if (point.AtEndOfDocument) {
+					break;
+				}
+			}
+
+			selection.MoveToPoint(point, extend);
+		}
+
+		static char GetChar(EditPoint point)
+		{
+			var str = point.GetText(1);
+			if (String.IsNullOrEmpty(str)) {
+				return ' ';
+			} else {
+				return str[0];
+			}
+		}
+
+		static bool IsCamelCase(char left, char right)
+		{
+			return Char.IsLetter(left) && (!Char.IsLetter(right) || (Char.IsLower(left) && Char.IsUpper(right)))
+					|| Char.IsDigit(left) && !Char.IsDigit(right)
+					|| Char.IsWhiteSpace(left) && !Char.IsWhiteSpace(right)
+					|| Char.IsPunctuation(left) && !Char.IsPunctuation(right);
+		}
+
+		static Regex toggleCommentRegex = new Regex(@"^(\s*)(//)(.*)", RegexOptions.Compiled);
+		[Command("Text Editor::Ctrl+Alt+C")]
+		static void ToggleComment(Connect connect)
+		{
+			var selection = connect.Application.ActiveDocument.Selection as TextSelection;
+
+			int firstLine = selection.TopPoint.Line;
+			int lastLine = selection.BottomPoint.Line;
+			var lineCount = 1 + lastLine - firstLine;
+
+			selection.GotoLine(firstLine, true);
+			selection.LineDown(true, lastLine - firstLine);
+			selection.EndOfLine(true);
+
+			var matches = new List<Match>(lineCount);
+			bool allLinesCommented = true;
+			for (var iline = firstLine; iline <= lastLine; iline++) {
+				selection.GotoLine(iline, true);
+				var match = toggleCommentRegex.Match(selection.Text);
+				matches.Add(match);
+				allLinesCommented &= match.Success;
+			}
+
+			if (allLinesCommented) {
+				for (var iline = firstLine; iline <= lastLine; iline++) {
+					var match = matches[iline - firstLine];
+					if (match.Success) {
+						selection.GotoLine(iline, true);
+						selection.Text = match.Groups[1].Value + match.Groups[3].Value;
+					}
+				}
+			} else {
+				for (var iline = firstLine; iline <= lastLine; iline++) {
+					var match = matches[iline - firstLine];
+					if (!match.Success) {
+						selection.GotoLine(iline, false);
+						selection.StartOfLine(vsStartOfLineOptions.vsStartOfLineOptionsFirstColumn);
+						selection.Text = "//";
+					}
+				}
+			}
+
+			selection.GotoLine(firstLine, true);
+			selection.LineDown(true, lastLine - firstLine);
+			selection.EndOfLine(true);
+		}
+
+		[Command("Text Editor::Alt+PgDn")]
+		static void HalfPageDown(Connect connect)
+		{
+			var selection = connect.Application.ActiveDocument.Selection as TextSelection;
+			int line = selection.ActivePoint.Line;
+			selection.PageDown(false);
+			int nextLine = selection.ActivePoint.Line;
+			selection.GotoLine((nextLine + line) >> 1);
+		}
+
+		[Command("Text Editor::Alt+PgUp")]
+		static void HalfPageUp(Connect connect)
+		{
+			var selection = connect.Application.ActiveDocument.Selection as TextSelection;
+			int line = selection.ActivePoint.Line;
+			selection.PageUp(false);
+			int nextLine = selection.ActivePoint.Line;
+			selection.GotoLine((nextLine + line) >> 1);
+		}
+
+		static Regex joinLinesRegex = new Regex(@"(\n|\r\n?|\r|\s{2,})", RegexOptions.Compiled | RegexOptions.Multiline);
+		[Command("Text Editor::Ctrl+Shift+J")]
+		static void JoinLines(Connect connect)
+		{
+			var selection = connect.Application.ActiveDocument.Selection as TextSelection;
+			var topLine = selection.TopPoint.Line;
+			var bottomLine = selection.BottomPoint.Line;
+			selection.GotoLine(topLine, true);
+			selection.LineDown(true, bottomLine - topLine);
+			selection.EndOfLine(true);
+
+			var text = selection.Text;
+			var firstSpace = joinLinesRegex.Match(text);
+			selection.Text = firstSpace + joinLinesRegex.Replace(text.Substring(firstSpace.Length), "");
+		}
+
+		[Command("Global::Alt+Shift+F1")]
+		static void Test(Connect connect)
+		{
+
+		}
+	}
+}
---
+++ Main/Connect.cs
@@ -0,0 +1,187 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+#if DEBUG
+namespace CommonAddInDebug
+#else
+namespace CommonAddIn
+#endif
+{
+	public class CommandAttribute : Attribute
+	{
+		public string name;
+		public string binding;
+
+		public CommandAttribute(string tbinding)
+		{
+			name = null;
+			binding = tbinding;
+		}
+
+		public CommandAttribute(string tname, string tbinding)
+		{
+			name = tname;
+			binding = tbinding;
+		}
+	}
+
+	public class Connect : IDTExtensibility2, IDTCommandTarget
+	{
+		private class CommandExec
+		{
+			public Command command;
+			public MethodInfo method;
+		}
+
+		private DTE2 application;
+		private AddIn addIn;
+		private Dictionary<string, CommandExec> commands = new Dictionary<string, CommandExec>();
+
+		public DTE2 Application { get { return application; } }
+		public AddIn AddIn { get { return addIn; } }
+
+		public void OnConnection(object tapplication, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+		{
+			application = (DTE2)tapplication;
+			addIn = (AddIn)addInInst;
+
+			Init();
+		}
+
+		public void OnDisconnection(ext_DisconnectMode disconnectMode, ref Array custom)
+		{
+		}
+
+		public void OnAddInsUpdate(ref Array custom)
+		{
+		}
+
+		public void OnStartupComplete(ref Array custom)
+		{
+
+		}
+
+		public void OnBeginShutdown(ref Array custom)
+		{
+		}
+
+		public void Exec(string CmdName, vsCommandExecOption ExecuteOption, ref object VariantIn, ref object VariantOut, ref bool Handled)
+		{
+			CommandExec result;
+			if (commands.TryGetValue(CmdName, out result)) {
+				try {
+					bool undoOpen = application.UndoContext.IsOpen;
+					if (!undoOpen) {
+						application.UndoContext.Open(CmdName);
+					}
+					result.method.Invoke(null, new object[] { this });
+					if (!undoOpen) {
+						application.UndoContext.Close();
+					}
+				} catch (Exception e) {
+					MessageBox.Show("Error at Exec: " + e.ToString());
+				}
+				Handled = true;
+			} else {
+				MessageBox.Show("Command not found: " + CmdName);
+				Handled = false;
+			}
+		}
+
+		public void QueryStatus(string CmdName, vsCommandStatusTextWanted NeededText, ref vsCommandStatus StatusOption, ref object CommandText)
+		{
+			StatusOption = vsCommandStatus.vsCommandStatusEnabled | vsCommandStatus.vsCommandStatusSupported;
+		}
+
+		void Init()
+		{
+			try {
+				StringBuilder builder = new StringBuilder();
+
+				var types = Assembly.GetExecutingAssembly().GetTypes();
+				foreach (var type in types) {
+					var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+					foreach (var method in methods) {
+						CommandAttribute attr = method.GetCustomAttribute<CommandAttribute>();
+						if (attr != null) {
+							AddCommand(attr.name == null ? method.Name : attr.name, attr.binding, method);
+						}
+					}
+				}
+
+			} catch (Exception e) {
+				MessageBox.Show("Error at Init: " + e.ToString());
+			}
+
+//			string result = "";
+//			foreach (var key in commands.Keys) {
+//				result += key + "\n";
+//			}
+//			MessageBox.Show(result);
+		}
+
+		void AddCommand(string name, string binding, MethodInfo method)
+		{
+			string fullName = addIn.ProgID + "." + name;
+			if (commands.ContainsKey(fullName)) {
+				MessageBox.Show("A command with the same name already exists: " + name);
+				return;
+			}
+
+			Command result = null;
+			var parameters = method.GetParameters();
+			if (!(parameters.Length == 1
+				&& parameters[0].ParameterType == typeof(Connect))) {
+				return;
+			}
+
+			try {
+				result = application.Commands.Item(fullName);
+			} catch (Exception) { }
+
+			if (result == null) {
+				try {
+					result = application.Commands.AddNamedCommand(addIn, name, name, name, false);
+				} catch (Exception) { MessageBox.Show("2"); }
+			}
+
+			if (result != null) {
+				try {
+					result.Bindings = binding;
+				} catch (Exception e) {
+					MessageBox.Show("Binding is invalid: " + binding + "\n" + e.ToString());
+				}
+
+				commands.Add(fullName, new CommandExec {
+					command = result,
+					method = method
+				});
+			}
+		}
+
+		bool BindCommand(string name, string binding)
+		{
+			Command command = null;
+			try {
+				command = application.Commands.Item(name);
+			} catch (Exception) { }
+
+			if (command != null) {
+				var bindings = command.Bindings as object[];
+				bindings.Concat(new object[] { binding });
+				command.Bindings = bindings;
+
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
